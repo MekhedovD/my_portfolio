e7ad7ed4bb358ca2904bd3b17ab7b0ed4065c8f4
Dmitry <mekhedov.d@mail.ru>
2021-07-19 18:41:15 +0300

    total #04

--- src/nav/burgerNav/BurgerNav.js
+++ src/nav/burgerNav/BurgerNav.js
@@ -1,46 +1,52 @@
-import React from "react";
+import React, {useState} from "react";
 import s from "./BurgerNav.module.scss";
-import { Link, animateScroll as scroll } from "react-scroll";
+import {Link} from "react-scroll";
 
 const BurgerNav = () => {
+  const [menuIsOpen, setBergerMenu] = useState(false);
+	const onBurgerBtnClick = () => {
+  	setBergerMenu(
+		  !menuIsOpen
+	  );
+		console.log(menuIsOpen)
+	}
 	return (
 		<div className={s.burgerNav}>
-			{/*<a href="" className="hreft">Main</a>*/}
-			{/*<a href="" className="hreft">My skills</a>*/}
-			<Link
-				activeClass={""}
-				to="main"
-				spy={true}
-				smooth={true}
-				offset={50}
-				duration={500}
-			>Main</Link>
-			<Link
-				activeClass={s.active}
-				to="skills"
-				spy={true}
-				smooth={true}
-				offset={50}
-				duration={500}
-			>My skills</Link>
-			<Link
-				activeClass={""}
-				to="projects"
-				spy={true}
-				smooth={true}
-				offset={50}
-				duration={500}
-			>My projects</Link>
-			<Link
-				activeClass={""}
-				to="contacts"
-				spy={true}
-				smooth={true}
-				offset={50}
-				duration={500}
-			>My contacts</Link>
-			{/*<a href="projects" className="hreft"></a>*/}
-			{/*<a href="" className="hreft">My contacts</a>*/}
+			<div className={menuIsOpen ? `${s.burgerNavItems} ${s.show}` : s.burgerNavItems}>
+				<Link
+					activeClass={""}
+					to="main"
+					spy={true}
+					smooth={true}
+					offset={50}
+					duration={500}
+				>Main</Link>
+				<Link
+					activeClass={s.active}
+					to="skills"
+					spy={true}
+					smooth={true}
+					offset={50}
+					duration={500}
+				>My skills</Link>
+				<Link
+					activeClass={""}
+					to="projects"
+					spy={true}
+					smooth={true}
+					offset={50}
+					duration={500}
+				>My projects</Link>
+				<Link
+					activeClass={""}
+					to="contacts"
+					spy={true}
+					smooth={true}
+					offset={50}
+					duration={500}
+				>My contacts</Link>
+			</div>
+			<div onClick={onBurgerBtnClick} className={s.burgerBtn}></div>
 		</div>
 	);
 }
