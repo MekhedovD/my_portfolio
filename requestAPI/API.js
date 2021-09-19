408cc80af3fe69e79458ed40228b7ab3fd419a63
Dmitry <mekhedov.d@mail.ru>
2021-09-19 21:20:53 +0300

    add request form

---
+++ requestAPI/API.js
@@ -0,0 +1,19 @@
+// import axios from "axios";
+//
+// export const instance = axios.create(
+// 	{
+// 		withCredentials: true,
+// 	})
+//
+// export const formAPI = {
+// 	addForm(name, contacts, message) {
+// 		return instance.post("http://localhost:3010/sendMessage", {dataForm: name, contacts, message})
+// 	}
+// }
+//
+// const addFormTC = (dataForm) => {
+// 	formAPI.addForm(dataForm)
+// 	.then(() => {
+// 		alert("You message send")
+// 	})
+// }
--- src/contacts/Contacts.js
+++ src/contacts/Contacts.js
@@ -1,26 +1,87 @@
-import React from "react";
+import React, {useState} from "react";
 import s from "./Contacts.module.scss";
 import sContainer from "../common/style/Container.module.css"
 import Title from "../common/componets/title/Title";
 import Fade from 'react-reveal/Fade';
+import {useFormik} from "formik";
+import * as Yup from "yup";
+import axios from "axios";
 
 
 const Contacts = () => {
+
+	const [passedModal, setPassedModal] = useState(false)
+	// const [failedModal, setFailedModal] = useState(false)
+
+	const formik = useFormik({
+		initialValues: {
+			name: '',
+			email: '',
+			message: '',
+		},
+		validationSchema: Yup.object({
+			name: Yup.string()
+			.max(20, 'Must be 20 characters or less')
+			.required('Name is required'),
+			email: Yup.string()
+			.email('Invalid email address')
+			.required('Email is required'),
+			message: Yup.string()
+			.required('Message is required'),
+		}),
+		onSubmit: values => {
+			alert(JSON.stringify(values));
+			axios.post('http://localhost:3010/sendMessage', values)
+			.then(() => {
+				setPassedModal(true)
+			})
+			formik.resetForm()
+		},
+	})
+
+
 	return (
 		<div className={s.contactsBlock} id={"contacts"}>
 			<Fade>
 				<div className={`${sContainer.container} ${s.contactsContainer}`}>
-					<Title text={"Contacts"} />
-					<form action="" className={s.form}>
-						<input type="text" placeholder={"Your name"} className={s.input}/>
-						<input type="text" placeholder={"Your email"} className={s.input}/>
-						<textarea name="" id="" placeholder={"Your message"} className={s.textarea}></textarea>
+					<Title text={"Contacts"}/>
+					<form
+						action=""
+						className={s.form}
+						onSubmit={formik.handleSubmit}
+					>
+						<input
+							className={s.input}
+							type="text"
+							placeholder={"Your name"}
+							// placeholder={{...formik.touched && formik.errors.name ? formik.errors.name : 'Your Name'}}
+							{...formik.getFieldProps("name")}
+							//{...formik.touched.name && formik.errors.name
+							//&& <div style={{'color': 'red'}}>{formik.errors.name}</div>}
+						/>
+						<input
+							className={s.input}
+							type="text"
+							placeholder={"Your email"}
+							{...formik.getFieldProps("email")}
+						/>
+						<textarea
+							placeholder={"Your message"}
+							className={s.textarea}
+							{...formik.getFieldProps("message")}
+						>
+						</textarea>
+						<button
+							type={"submit"}
+							value={"Send message"}
+							className={s.button}
+						>Send message
+						</button>
 					</form>
-					<button type={"submit"} className={s.button}>Send message</button>
 				</div>
 			</Fade>
 		</div>
-);
+	);
 }
 
 export default Contacts;
